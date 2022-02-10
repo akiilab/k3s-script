bd3908837ecebe21635cc737e280ac45b6d65f7d
Akiicat <aaaa1379@gmail.com>
2022-02-10 12:57:50 +0800

    update server.sh

--- server.sh
+++ server.sh
@@ -9,5 +9,13 @@ OPTIONS=$2
 
 : "${NODE_NAME:?Variable not set or empty}"
 
-sudo K3S_TOKEN=SECRET k3s server --node-name="$NODE_NAME" --write-kubeconfig-mode=644 $OPTIONS
+# default
+# sudo K3S_TOKEN=SECRET k3s server --node-name="$NODE_NAME" --write-kubeconfig-mode=644 $OPTIONS
+
+# calico
+# sudo K3S_TOKEN=SECRET k3s server --node-name="$NODE_NAME" --write-kubeconfig-mode=644 --flannel-backend=none --cluster-cidr=192.168.0.0/16 --disable-network-policy --disable=traefik $OPTIONS
+
+# Issue: FailedScheduling didn't have free ports
+# https://github.com/kubernetes/kubernetes/issues/66568#issuecomment-1002253558
+sudo K3S_TOKEN=SECRET k3s server --node-name="$NODE_NAME" --write-kubeconfig-mode=644 --disable=traefik --disable=servicelb
 
