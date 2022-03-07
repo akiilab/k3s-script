d1591276b4c5db5528468e06704744503a8bb95e
Akiicat <aaaa1379@gmail.com>
2022-03-07 16:34:01 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 sudo k3s-killall.sh
 
 sudo rm -rf /etc/rancher/k3s
@@ -7,3 +9,11 @@ sudo rm -rf /run/k3s
 sudo rm -rf /run/flannel
 sudo rm -rf /var/lib/rancher/k3s
 sudo rm -rf /var/lib/kubelet
+# sudo rm -rf /var/lib/cni
+sudo rm -rf /var/etcd
+
+# calico
+sudo rm -rf /etc/cni/net.d
+
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
